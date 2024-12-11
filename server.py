2da1a9323239d73fe8c2109e50350f37721f73ca
Matthew Wakeham <mwakeham95@gmail.com>
2024-12-10 21:45:31 -0600

    server, css

--- server.py
+++ server.py
@@ -1,23 +1,40 @@
 # server.py
 
+# Imports
 import json
 
 import os
 from dotenv import load_dotenv
 import requests
+from flask_sqlalchemy import SQLAlchemy
 from authlib.integrations.flask_client import OAuth
 from flask import Flask, abort, redirect, render_template, session, url_for
 
 # Load environment variables from the .env file in the variables folder
 load_dotenv(dotenv_path="variables/.env")
 
+# Create app
 app = Flask(__name__)
 
 # Configuration from environment variables
 app.secret_key = os.getenv("FLASK_SECRET")
 
+# Configure SQL Alchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+# Database Model ~ Single Row with our DB
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    email = db.Column(db.String(254), unique=True, nullable=False)
+    user_color = db.Column(db.String(7), unique=False, nullable=False)
+
+# Google data
 SERVER_METADATA_URL = "https://accounts.google.com/.well-known/openid-configuration"
 
+# OAuth credentials
 oauth = OAuth(app)
 oauth.register(
     "myApp",
@@ -30,11 +47,12 @@ oauth.register(
 )
 
 # Routes
+# Home
 @app.route("/")
 def home():
-    return render_template("home.html", session=session.get("user"),
-                           pretty=json.dumps(session.get("user"), indent=4))
+    return render_template("home.html", session=session.get("user"))
 
+# Sign in
 @app.route("/signin-google")
 def googleCallback():
     # fetch access token and id token using authorization code
@@ -49,16 +67,29 @@ def googleCallback():
     session["user"] = token
     return redirect(url_for("home"))
 
+# Login
 @app.route("/google-login")
 def googleLogin():
     if "user" in session:
         return redirect(url_for("home"))
     return oauth.myApp.authorize_redirect(redirect_uri=url_for("googleCallback", _external=True))
 
+# Logout
 @app.route("/logout")
 def logout():
     session.pop("user", None)
     return redirect(url_for("home"))
 
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+@app.route("/tos")
+def tos():
+    return render_template("tos.html")
+
+# Run the script directly 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=8000, debug=True)
